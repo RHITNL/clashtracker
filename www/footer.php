9a1eca81cf2eb53a5e309c518751bea9586db78b
Alex Inman <alexinmann@gmail.com>
2016-03-05 14:34:50 -0400

    Added space on top of a footer so that it wouldn't cover the page content

--- www/footer.php
+++ www/footer.php
@@ -1,6 +1,6 @@
 		</div>
 	</body>
-	<br><br>
+	<div style="margin-top: 50px">&nbsp;</div>
 	<footer class="footer" style="height: auto;">
 		<p class="text-muted text-center" style="margin-top: 10px;">This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it. <br>For more information see Supercell’s Fan Content Policy: <a href="http://www.supercell.com/fan-content-policy">www.supercell.com/fan-content-policy</a>.</p>
 	</footer>
