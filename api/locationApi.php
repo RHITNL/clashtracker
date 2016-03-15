4c3990350c6196e7b0d9a171a7e120c2b1d770fa
Alex Inman <alexinmann@gmail.com>
2016-03-15 11:47:05 -0300

    Added Location API.

---
+++ api/locationApi.php
@@ -0,0 +1,22 @@
+<?
+class locationApi extends api{
+	public function getLocations(){
+		$extension = 'locations';
+		return $this->request($extension);
+	}
+
+	public function getLocation($id){
+		$extension = 'locations/' . urlencode($id);
+		return $this->request($extension);
+	}
+
+	public function getLocationRankingClans($id){
+		$extension = 'locations/' . urlencode($id) . '/rankings/clans';
+		return $this->request($extension)->items;
+	}
+
+	public function getLocationRankingPlayers($id){
+		$extension = 'locations/' . urlencode($id) . '/rankings/players';
+		return $this->request($extension)->items;
+	}
+}
