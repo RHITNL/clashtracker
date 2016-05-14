2c803b4645c2a8df24c7c5c7de49cc04f5f487d7
Alex Inman <alexinmann@gmail.com>
2016-05-14 17:02:34 -0300

    Implemented #17. Now forces users to have stronger passwords

--- www/processForgotPassword.php
+++ www/processForgotPassword.php
@@ -30,7 +30,7 @@ $link = "http://" . $_SERVER['HTTP_HOST'] . "/login.php";
 $subject = "Forgotten Password";
 $message = "Hello,\n\n\tWe have received a request to reset the password on your Clash Tracker account. Your new password is " . $newPassword . ". We recommend changing you password immediately after using this one to sign in. You can click on the below link to sign in now: " . $link . "\nPlease do not reply to this email.\nClash on,\n\nClash&nbsp;Tracker Account Support\n";
 if(email($email, $subject, $message, 'password@clashtracker.ca')){
-	$_SESSION['curMessage'] = 'Password reset email successfully sent.';
+	$_SESSION['curMessage'] = 'Password reset email successfully sent. It may take a few minutes to arrive.';
 	header('Location: /login.php');
 }else{
 	$_SESSION['curError'] = 'There was an error trying to reset your password. Please email alexinmann@gmail.com for help.';
