ea7e955a8d69ab7afe337117c166494614da7944
Alex Inman <alexinmann@gmail.com>
2016-05-28 01:14:15 -0300

    Fixed sql problems

--- sql/2016-05-25.sql
+++ sql/2016-05-25.sql
@@ -42,14 +42,6 @@ begin
 end //
 delimiter ;
 
-drop procedure if exists p_player_get_wars;
-delimiter //
-create procedure p_player_get_wars(varPlayerId int)
-begin
-	select war.*, clan1.name as first_clan_name, clan2.name as second_clan_name from war_player join war on war.id = war_player.war_id join clan as clan1 on war.first_clan_id = clan1.id join clan as clan2 on war.second_clan_id = clan2.id where player_id = varPlayerId order by date_created desc;
-end //
-delimiter ;
-
 drop procedure if exists p_get_wars;
 delimiter //
 create procedure p_get_wars(varPageSize int)
@@ -68,18 +60,7 @@ begin
 end //
 delimiter ;
 
-drop table proxy_requests;
-
-drop procedure if exists p_war_get_players;
-delimiter //
-create procedure p_war_get_players(varWarId int, varClanId int)
-begin
-if (varClanId = '%')
-	then select player.*, war_player.rank from player join war_player on war_player.player_id = player.id where war_player.war_id = varWarId order by war_player.rank;
-	else select player.*, war_player.rank from player join war_player on war_player.player_id = player.id where war_player.war_id = varWarId and clan_id = varClanId order by war_player.rank;
-end if;
-end //
-delimiter ;
+drop table if exists proxy_requests;
 
 drop procedure if exists p_player_create;
 delimiter //
@@ -88,8 +69,6 @@ begin
 	insert into player(name, tag, date_created) values(varName, varTag, varDate);
 	select * from player where id in (select last_insert_id() as id);
 end //
-delimiter ;
-
 delimiter ;
 
 drop procedure if exists p_player_get_wars;
@@ -100,26 +79,6 @@ begin
 end //
 delimiter ;
 
-drop procedure if exists p_get_wars;
-delimiter //
-create procedure p_get_wars(varPageSize int)
-begin
-	select war.*, clan1.name as first_clan_name, clan2.name as second_clan_name from war join clan as clan1 on war.first_clan_id = clan1.id join clan as clan2 on war.second_clan_id = clan2.id order by date_created desc limit varPageSize;
-end //
-delimiter ;
-
-drop procedure if exists p_get_players_and_clans_from_tags;
-delimiter //
-create procedure p_get_players_and_clans_from_tags(in varTags varchar(1000))
-begin
-  set @st := concat('select player.*, clan_member.clan_id, rank from player left join clan_member on player.id = clan_member.player_id and clan_member.rank != 5 where tag in ', varTags, ';');
-  prepare stmt from @st;
-  execute stmt;
-end //
-delimiter ;
-
-drop table proxy_requests;
-
 drop procedure if exists p_war_get_players;
 delimiter //
 create procedure p_war_get_players(varWarId int, varClanId int)
@@ -128,15 +87,6 @@ if (varClanId = '%')
 	then select player.*, war_player.rank from player join war_player on war_player.player_id = player.id where war_player.war_id = varWarId order by war_player.rank;
 	else select player.*, war_player.rank from player join war_player on war_player.player_id = player.id where war_player.war_id = varWarId and clan_id = varClanId order by war_player.rank;
 end if;
-end //
-delimiter ;
-
-drop procedure if exists p_player_create;
-delimiter //
-create procedure p_player_create(varName varchar(50), varTag varchar(15), varDate datetime)
-begin
-	insert into player(name, tag, date_created) values(varName, varTag, varDate);
-	select * from player where id in (select last_insert_id() as id);
 end //
 delimiter ;
 
