947e518bf0f5fadb84ba6cac0df54cfcf7e6fc65
Alex Inman <alexinmann@gmail.com>
2016-07-09 13:24:40 -0300

    Fixes #44 - now checks to see if records exist as well and starts the stat collection when it doesn't

---
+++ sql/2016-07-09.sql
@@ -0,0 +1,48 @@
+drop procedure if exists p_player_update_bulk;
+delimiter //
+create procedure p_player_update_bulk(varId int, varRank varchar(2), varLevel int, varTrophies int, varDonations int, varReceived int, varLeagueUrl varchar(200), varDate datetime)
+begin
+    update player set level=varLevel, trophies=varTrophies, donations=varDonations, received=varReceived, league_url=varLeagueUrl where id = varId;
+    update clan_member set rank=varRank where player_id = varId and rank != 5;
+    if (varLevel <> (select stat_amount from player_stats where player_id = varId and stat_type = 'LV' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'LV' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'LV', varLevel);
+    end if;
+    if (varTrophies <> (select stat_amount from player_stats where player_id = varId and stat_type = 'TR' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'TR' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'TR', varTrophies);
+    end if;
+    if (varDonations <> (select stat_amount from player_stats where player_id = varId and stat_type = 'DO' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'DO' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'DO', varDonations);
+    end if;
+    if (varReceived <> (select stat_amount from player_stats where player_id = varId and stat_type = 'RE' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'RE' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'RE', varReceived);
+    end if;
+end //
+delimiter ;
+
+drop procedure if exists p_clan_update_bulk;
+delimiter //
+create procedure p_clan_update_bulk(varId int, varName varchar(50), varType varchar(2), varDescription varchar(256), varFrequency varchar(2), varMinTrophies int, varMembers int, varClanPoints int, varClanLevel int, varWarWins int, varBadgeUrl varchar(200), varLocation varchar(50), varDateModified datetime, varHourAgo datetime)
+    begin
+        update clan set name=varName, clan_type=varType, description=varDescription, war_frequency=varFrequency, minimum_trophies=varMinTrophies, members=varMembers, clan_points=varClanPoints, clan_level=varClanLevel, war_wins=varWarWins, badge_url=varBadgeUrl, location=varLocation, date_modified=varDateModified where id = varId;
+        if (varClanPoints <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CP' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'CP' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CP', varClanPoints);
+        end if;
+        if (varClanLevel <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CL' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'CL' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CL', varClanLevel);
+        end if;
+        if (varMembers <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'ME' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'ME' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'ME', varMembers);
+        end if;
+        if (varWarWins <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'WW' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'WW' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'WW', varWarWins);
+        end if;
+    end //
+delimiter ;
--- sql/master.sql
+++ sql/master.sql
@@ -926,27 +926,6 @@ end if;
 end //
 delimiter ;
 
-drop procedure if exists p_player_update_bulk;
-delimiter //
-create procedure p_player_update_bulk(varId int, varRank varchar(2), varLevel int, varTrophies int, varDonations int, varReceived int, varLeagueUrl varchar(200), varDate datetime)
-begin
-	update player set level=varLevel, trophies=varTrophies, donations=varDonations, received=varReceived, league_url=varLeagueUrl where id = varId;
-	update clan_member set rank=varRank where player_id = varId and rank != 5;
-	if (varLevel <> (select stat_amount from player_stats where player_id = varId and stat_type = 'LV' order by date_recorded desc limit 1))
-		then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'LV', varLevel);
-	end if;
-	if (varTrophies <> (select stat_amount from player_stats where player_id = varId and stat_type = 'TR' order by date_recorded desc limit 1))
-		then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'TR', varTrophies);
-	end if;
-	if (varDonations <> (select stat_amount from player_stats where player_id = varId and stat_type = 'DO' order by date_recorded desc limit 1))
-		then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'DO', varDonations);
-	end if;
-	if (varReceived <> (select stat_amount from player_stats where player_id = varId and stat_type = 'RE' order by date_recorded desc limit 1))
-		then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'RE', varReceived);
-	end if;
-end //
-delimiter ;
-
 drop procedure if exists p_player_leave_clan;
 delimiter //
 create procedure p_player_leave_clan(varPlayerId int, varDate datetime)
@@ -1063,27 +1042,6 @@ create procedure p_player_delete_record(varId int, varType varchar(2))
 delimiter ;
 
 alter table clan drop column api_info;
-
-drop procedure if exists p_clan_update_bulk;
-delimiter //
-create procedure p_clan_update_bulk(varId int, varName varchar(50), varType varchar(2), varDescription varchar(256), varFrequency varchar(2), varMinTrophies int, varMembers int, varClanPoints int, varClanLevel int, varWarWins int, varBadgeUrl varchar(200), varLocation varchar(50), varDateModified datetime, varHourAgo datetime)
-	begin
-		update clan set name=varName, clan_type=varType, description=varDescription, war_frequency=varFrequency, minimum_trophies=varMinTrophies, members=varMembers, clan_points=varClanPoints, clan_level=varClanLevel, war_wins=varWarWins, badge_url=varBadgeUrl, location=varLocation, date_modified=varDateModified where id = varId;
-		if (varClanPoints <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CP' order by date_recorded desc limit 1))
-		then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CP', varClanPoints);
-		end if;
-		if (varClanLevel <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CL' order by date_recorded desc limit 1))
-		then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CL', varClanLevel);
-		end if;
-		if (varMembers <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'ME' order by date_recorded desc limit 1))
-		then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'ME', varMembers);
-		end if;
-		if (varWarWins <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'WW' order by date_recorded desc limit 1))
-		then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'WW', varWarWins);
-		end if;
-	end //
-delimiter ;
-
 alter table war add column stars_locked boolean;
 alter table war add column first_clan_destruction float;
 alter table war add column second_clan_destruction float;
@@ -1162,4 +1120,53 @@ create procedure p_get_players(varOrder varchar(50), varPageSize int)
 		prepare stmt from @st;
 		execute stmt;
 	end //
+delimiter ;
+
+drop procedure if exists p_player_update_bulk;
+delimiter //
+create procedure p_player_update_bulk(varId int, varRank varchar(2), varLevel int, varTrophies int, varDonations int, varReceived int, varLeagueUrl varchar(200), varDate datetime)
+begin
+    update player set level=varLevel, trophies=varTrophies, donations=varDonations, received=varReceived, league_url=varLeagueUrl where id = varId;
+    update clan_member set rank=varRank where player_id = varId and rank != 5;
+    if (varLevel <> (select stat_amount from player_stats where player_id = varId and stat_type = 'LV' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'LV' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'LV', varLevel);
+    end if;
+    if (varTrophies <> (select stat_amount from player_stats where player_id = varId and stat_type = 'TR' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'TR' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'TR', varTrophies);
+    end if;
+    if (varDonations <> (select stat_amount from player_stats where player_id = varId and stat_type = 'DO' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'DO' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'DO', varDonations);
+    end if;
+    if (varReceived <> (select stat_amount from player_stats where player_id = varId and stat_type = 'RE' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'RE' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'RE', varReceived);
+    end if;
+end //
+delimiter ;
+
+drop procedure if exists p_clan_update_bulk;
+delimiter //
+create procedure p_clan_update_bulk(varId int, varName varchar(50), varType varchar(2), varDescription varchar(256), varFrequency varchar(2), varMinTrophies int, varMembers int, varClanPoints int, varClanLevel int, varWarWins int, varBadgeUrl varchar(200), varLocation varchar(50), varDateModified datetime, varHourAgo datetime)
+    begin
+        update clan set name=varName, clan_type=varType, description=varDescription, war_frequency=varFrequency, minimum_trophies=varMinTrophies, members=varMembers, clan_points=varClanPoints, clan_level=varClanLevel, war_wins=varWarWins, badge_url=varBadgeUrl, location=varLocation, date_modified=varDateModified where id = varId;
+        if (varClanPoints <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CP' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'CP' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CP', varClanPoints);
+        end if;
+        if (varClanLevel <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'CL' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'CL' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'CL', varClanLevel);
+        end if;
+        if (varMembers <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'ME' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'ME' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'ME', varMembers);
+        end if;
+        if (varWarWins <> (select stat_amount from clan_stats where clan_id = varId and stat_type = 'WW' order by date_recorded desc limit 1)
+            or not exists (select * from clan_stats where clan_id = varId and stat_type = 'WW' limit 1))
+        then insert into clan_stats (clan_id, date_recorded, stat_type, stat_amount) values (varId, varDateModified, 'WW', varWarWins);
+        end if;
+    end //
 delimiter ;
