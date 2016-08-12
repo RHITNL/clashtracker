4eebbd6d4ef12c52c5798df046c95e3f70ffb30a
Alex Inman <alexinmann@gmail.com>
2016-08-12 00:28:28 -0300

    Fixed the p_war_delete sql procedure

--- sql/2016-06-11.sql
+++ sql/2016-06-11.sql
@@ -2,8 +2,10 @@ drop procedure if exists p_war_delete;
 delimiter //
 create procedure p_war_delete(varId int)
 begin
-	delete from clan where id = varId;
-	delete from war_player where war_id = varId;
+	delete from war_edit_requests where war_id = varId;
+	delete from war_allowed_users where war_id = varId;
 	delete from war_attack where war_id = varId;
+	delete from war_player where war_id = varId;
+	delete from war where id = varId;
 end //
 delimiter ;
--- sql/master.sql
+++ sql/master.sql
@@ -1200,3 +1200,15 @@ alter table player add stars_on_defence int default 0;
 alter table player add number_of_defences int default 0;
 alter table player add attacks_used int default 0;
 alter table player add number_of_wars int default 0;
+
+drop procedure if exists p_war_delete;
+delimiter //
+create procedure p_war_delete(varId int)
+begin
+	delete from war_edit_requests where war_id = varId;
+	delete from war_allowed_users where war_id = varId;
+	delete from war_attack where war_id = varId;
+	delete from war_player where war_id = varId;
+	delete from war where id = varId;
+end //
+delimiter ;
